eb479081ce02bdd4494ab1e83c8e90d46ae50b28
Christina Nkya <christina.nkya@sdgku.edu>
2024-09-04 21:29:09 -0400

    Added password capes

--- issues/views.py
+++ issues/views.py
@@ -17,17 +17,31 @@ class IssueListView(LoginRequiredMixin, ListView):    #scan operation
     template_name = "issues/list.html"
     model = Issue
 
+    
+
 class IssueDetailView(LoginRequiredMixin, DetailView):
     template_name = "issues/detail.html"
     model = Issue
+
+    def test_func(self):
+        post = self.get_object()
+        return issue.reporter == self.request.user
 
 class IssueCreateView(LoginRequiredMixin, CreateView):
     template_name = "issues/new.html"
     model = Issue
     fields = ["summary", "status", "description", "reporter"  ]
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 class IssueUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "issues/update.html"
     model = Issue
     fields = ["summary", "status", "priority", "description", "reporter", "user" ]
+
+    def test_func(self):
+        post = self.get_object()
+        return issue.reporter == self.request.user
     
